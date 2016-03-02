0bb11abfc67de2017f65f05d1bf4e43d334b10ba
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2016-03-01 23:37:50 -0500

    Flushed out MatchReport

--- src/us/andrewdickinson/frc/scoutingproccessing/MatchReport.java
+++ src/us/andrewdickinson/frc/scoutingproccessing/MatchReport.java
@@ -1,26 +1,51 @@
 package us.andrewdickinson.frc.scoutingproccessing;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import com.itextpdf.text.DocumentException;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Created by Andrew on 2/28/16.
  */
 public class MatchReport {
+    int match;
     ArrayList<TeamReport> redTeams;
     ArrayList<TeamReport> blueTeams;
 
     public MatchReport(HashMap<Integer, Team> allTeams, int match){
+        this.match = match;
         TBACommunication tbaCommunication = TBACommunication.getInstance();
         int[] redTeams = tbaCommunication.getRedTeams(match);
         int[] blueTeams = tbaCommunication.getBlueTeams(match);
 
+        this.redTeams = new ArrayList<>();
+        this.blueTeams = new ArrayList<>();
+
         for (int teamNum : redTeams){
-            this.redTeams.add(new TeamReport(allTeams.get(teamNum)));
+            if (allTeams.get(teamNum) != null) this.redTeams.add(new TeamReport(allTeams.get(teamNum)));
         }
 
         for (int teamNum : blueTeams){
-            this.blueTeams.add(new TeamReport(allTeams.get(teamNum)));
+            if (allTeams.get(teamNum) != null) this.blueTeams.add(new TeamReport(allTeams.get(teamNum)));
         }
+    }
+
+    public void generateMatchPDF() throws IOException, DocumentException{
+        for (TeamReport teamReport : redTeams) {
+            teamReport.generatePDF("reports/team/");
+        }
+
+        for (TeamReport teamReport : blueTeams) {
+            teamReport.generatePDF("reports/team/");
+        }
+
+        List<Integer> bothAlliances = IntStream.concat(redTeams.stream().mapToInt(TeamReport::getTeamNumber),
+                blueTeams.stream().mapToInt(TeamReport::getTeamNumber)).boxed().collect(Collectors.toList());
+
+        ReportGenerator.mergeTeams("reports/team/", "reports/match/" + match + ".pdf", new ArrayList<>(bothAlliances));
     }
 }
