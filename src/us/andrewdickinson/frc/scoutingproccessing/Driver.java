8fb32472385a9eca20ea25c58db9a605f28ad26f
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2016-03-01 23:38:51 -0500

    Added some stuff to driver

--- src/us/andrewdickinson/frc/scoutingproccessing/Driver.java
+++ src/us/andrewdickinson/frc/scoutingproccessing/Driver.java
@@ -1,5 +1,7 @@
 package us.andrewdickinson.frc.scoutingproccessing;
 
+import com.itextpdf.text.DocumentException;
+
 import java.io.IOException;
 import java.util.*;
 
@@ -7,7 +9,27 @@ import java.util.*;
  * Created by Andrew on 2/28/16.
  */
 public class Driver {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, DocumentException {
+        HashMap<Integer, Team> teams = Importer
+                .importAllData("/home/Andrew/gdrive/Master_Spreadsheet/Master_Spreadsheet.xlsx");
+//        Team t = teams.get(4098);
+//        TeamReport tr = new TeamReport(t);
+//
+//        ReportGenerator rg = new ReportGenerator(tr, "reports/team/");
+//        rg.generate();
+//
+//        t = teams.get(2386);
+//        tr = new TeamReport(t);
+//
+//        rg = new ReportGenerator(tr, "reports/team/");
+//        rg.generate();
+//
+//        ArrayList<Integer> team_nums = new ArrayList<>();
+//        team_nums.add(4098);
+//        team_nums.add(2386);
+//        ReportGenerator.mergeTeams("reports/team/", team_nums);
 
+        MatchReport mr = new MatchReport(teams, 1);
+        mr.generateMatchPDF();
     }
 }
