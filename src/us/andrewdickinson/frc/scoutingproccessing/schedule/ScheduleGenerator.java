10e8832c9faacde5d5a9c07147ba356992357133
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2016-03-09 19:04:12 -0500

    Added schedule generation classes

---
+++ src/us/andrewdickinson/frc/scoutingproccessing/schedule/ScheduleGenerator.java
@@ -0,0 +1,73 @@
+package us.andrewdickinson.frc.scoutingproccessing.schedule;
+
+import us.andrewdickinson.frc.scoutingproccessing.TBACommunication;
+
+import java.util.*;
+
+/**
+ * Created by Andrew on 3/8/16.
+ */
+public class ScheduleGenerator {
+    private TBACommunication tba;
+    private ScoutingSchedule scoutingSchedule;
+
+    public static final boolean RANDOM_ANEALING_ENABLED = true;
+
+    public ScheduleGenerator(){
+        tba = TBACommunication.getInstance();
+        scoutingSchedule = new ScoutingSchedule(tba);
+    }
+
+    public void generate(){
+        for (int i = 0; i < 10000; i++) {
+            runRound();
+        }
+    }
+
+    private void runRound(){
+        double original_score = scoutingSchedule.getScore();
+
+        double average_frequency = scoutingSchedule.getAverageMatchesPerTeam();
+
+        List<Integer> teamsToTryReorganizing = scoutingSchedule.getTeamsOverAverage();
+        for (int team : teamsToTryReorganizing){
+            for (int i = 1; i < scoutingSchedule.numberOfMatches() + 1; i++){
+                if (Arrays.stream(scoutingSchedule.getMatch(i)).anyMatch(t -> t == team)) {
+                    Integer last_replacement = null;
+                    int[] replacements = scoutingSchedule.getTeamReplacementOptions(i);
+                    for (int replacement : replacements){
+                        if (last_replacement == null) {
+                            last_replacement = replacement;
+                            scoutingSchedule.replaceTeamInMatch(i, team, replacement);
+                        } else {
+                            scoutingSchedule.replaceTeamInMatch(i, last_replacement, replacement);
+                        }
+
+                        double loop_score = scoutingSchedule.getScore();
+                        if (loop_score <= original_score
+                                || randomMapDeltaToKeep(loop_score - original_score)){
+                                    original_score = loop_score;
+                        } else {
+                            //Undo the change we just made
+                            last_replacement = null;
+                            scoutingSchedule.replaceTeamInMatch(i, replacement, team);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public boolean randomMapDeltaToKeep(double delta){
+        double probability = Math.pow(Math.E, (-500 * Math.abs(delta) - 1));
+        if (RANDOM_ANEALING_ENABLED){
+            return Math.random() < probability;
+        } else {
+            return false;
+        }
+    }
+
+    public ScoutingSchedule getScoutingSchedule() {
+        return scoutingSchedule;
+    }
+}
---
+++ src/us/andrewdickinson/frc/scoutingproccessing/schedule/ScoutingSchedule.java
@@ -0,0 +1,169 @@
+package us.andrewdickinson.frc.scoutingproccessing.schedule;
+
+import us.andrewdickinson.frc.scoutingproccessing.TBACommunication;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Andrew on 3/9/16.
+ */
+public class ScoutingSchedule {
+    private List<int[]> matches;
+    private ArrayList<int[]> scoutingMatches;
+
+    public ScoutingSchedule(TBACommunication tba){
+        matches = new ArrayList<>();
+        scoutingMatches = new ArrayList<>();
+
+        if (tba.scheduleGenerated()){
+            for (int i = 1; i <= tba.getNumberOfMatches(); i++){
+                if (tba.isMatchNotOnLastDay(i)) matches.add(tba.getTeams(i));
+            }
+        }
+
+        for (int[] match : matches){
+            Random r = new Random();
+            List<Integer> matchList = Arrays.stream(match).boxed().collect(Collectors.toList());
+            int[] match_reduced = new int[4];
+            for (int i = 0; i < 4; i++){
+                int team = r.nextInt(6 - i);
+                match_reduced[i] = matchList.remove(team);
+            }
+            scoutingMatches.add(match_reduced);
+        }
+    }
+
+    protected int[] getMatch(int match){
+        return scoutingMatches.get(match - 1);
+    }
+
+    public String toString(){
+        String out = "";
+        for (int[] match : scoutingMatches){
+            for (int team : match){
+                out += team + ", ";
+            }
+            out += "\n";
+        }
+
+        return out;
+    }
+
+    public double getScore(){
+        Map<Integer, Integer> frequencyMap = getFrequencyMap();
+        List<Integer> freqs = frequencyMap.entrySet().stream().map(Map.Entry::getValue).collect(Collectors.toList());
+
+        //Compute the variance
+        double sum = 0.0;
+        double size = freqs.size();
+        for(double a : freqs)
+            sum += a;
+        double mean = sum/size;
+
+        double temp = 0;
+        for(double a :freqs) {
+            temp += (mean - a) * (mean - a);
+        }
+
+        return temp/size;
+    }
+
+    public Map<Integer, Integer> getDistribution(){
+        Map<Integer, Integer> frequencyMap = getFrequencyMap();
+        Map<Integer, Integer> distribution = new HashMap<>();
+        List<Integer> freqs = frequencyMap.entrySet().stream().map(Map.Entry::getValue).collect(Collectors.toList());
+
+        for (int matches : freqs){
+            if (distribution.containsKey(matches)){
+                distribution.put(matches, distribution.get(matches) + 1);
+            } else {
+                distribution.put(matches, 1);
+            }
+        }
+
+        return distribution;
+    }
+
+    public double getAverageMatchesPerTeam(){
+        Map<Integer, Integer> frequencyMap = getFrequencyMap();
+        List<Integer> freqs = frequencyMap.entrySet().stream().map(Map.Entry::getValue).collect(Collectors.toList());
+
+        //Compute the variance
+        double sum = 0.0;
+        double size = freqs.size();
+        for(double a : freqs)
+            sum += a;
+        return sum/size;
+    }
+
+    public Map<Integer, Integer> getFrequencyMap(){
+        Map<Integer, Integer> numberOfMatchesDistribution = new HashMap<>(45);
+        for (int[] match : matches){
+            for (int team : match){
+                if (numberOfMatchesDistribution.containsKey(team)){
+                    numberOfMatchesDistribution.put(team, numberOfMatchesDistribution.get(team) + 1);
+                } else {
+                    numberOfMatchesDistribution.put(team, 1);
+                }
+            }
+        }
+
+        return numberOfMatchesDistribution;
+    }
+
+    protected void replaceTeamInMatch(int match, int find, int replace){
+        for (int i = 0; i < scoutingMatches.get(match - 1).length; i++){
+            if (scoutingMatches.get(match - 1)[i] == find){
+                scoutingMatches.get(match - 1)[i] = replace;
+                return;
+            }
+        }
+
+        throw new IllegalArgumentException("Team: " + find + " not found for given match");
+    }
+
+    public int[] getTeamReplacementOptions(int match){
+        List<Integer> options = Arrays.stream(matches.get(match - 1)).boxed().collect(Collectors.toList());
+        options.removeAll(Arrays.stream(scoutingMatches.get(match - 1)).boxed().collect(Collectors.toList()));
+        return options.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public List<Integer> getTeamsOverAverage(){
+        Map<Integer, Integer> frequencyMap = getFrequencyMap();
+        double average_matches = getAverageMatchesPerTeam();
+        return frequencyMap.entrySet().stream()
+                .filter(entry -> entry.getValue() > average_matches)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+    }
+
+    public int numberOfMatches(){
+        return scoutingMatches.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ScoutingSchedule that = (ScoutingSchedule) o;
+
+        for (int i = 0; i < scoutingMatches.size(); i++){
+            if (!Arrays.equals(this.scoutingMatches.get(i), that.scoutingMatches.get(i))) return false;
+        }
+
+        for (int i = 0; i < matches.size(); i++){
+            if (!Arrays.equals(this.matches.get(i), that.matches.get(i))) return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = matches.hashCode();
+        result = 31 * result + scoutingMatches.hashCode();
+        return result;
+    }
+}
